4c2bffcbb75cea1516c6c978704af081059ca485
Patinyaporn Sudtho <cosyprang@gmail.com>
2025-03-08 01:29:30 +0700

    fix responsive FHD SiteBar

--- src/components/Home/SiteBar/SiteBar.jsx
+++ src/components/Home/SiteBar/SiteBar.jsx
@@ -4,27 +4,29 @@ import '../SiteBar/SiteBar.css'
 function SiteBar() {
     return (
         <>
-            <section className="sidebar-section">
-                <div className="sidebar-container">
-                    <div className="sidebar-wrapper">
-                        <div className="sidebar-title-col">
-                            <div className="sidebar-title-wrapper">
+            <section className="sidebar-container">
+                <div className="sidebar-wrapper">
+                    <div className="sidebar-title-col">
+                        <div className="sidebar-title-wrapper">
+                            <div className="title-header">
                                 <h1><a href="#">MAPRANG</a></h1>
                                 <p>Computer Science Student</p>
                             </div>
-                        </div>
-                        <div className="sidebar-info-col">
-                            <div className="sidebar-info-wrapper">
+                            <div className="title-name">
                                 <h2>My name is <br />Patinyaporn Sudtho.</h2>
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque laudantium nostrum facere quam voluptas, ipsam numquam reprehenderit ullam perspiciatis corporis aliquam, assumenda est. Quidem consequuntur animi quasi ea blanditiis tempore?</p>
                             </div>
                         </div>
-                        <div className="sidebar-contact-col">
-                            <div className="sidebar-contact-wrapper">
-                                <p className="tel-contact">+66 92 421 8855</p>
-                                <p className="mail-contact">cosyprang@gmail.com</p>
-                                <a href="#" className="btn-contact">See my Portfolio</a>
-                            </div>
+                    </div>
+                    <div className="sidebar-info-col">
+                        <div className="sidebar-info-wrapper">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque laudantium nostrum facere quam voluptas, ipsam numquam reprehenderit ullam perspiciatis corporis aliquam, assumenda est. Quidem consequuntur animi quasi ea blanditiis tempore?</p>
+                        </div>
+                    </div>
+                    <div className="sidebar-contact-col">
+                        <div className="sidebar-contact-wrapper">
+                            <p className="tel-contact">+66 92 421 8855</p>
+                            <p className="mail-contact">cosyprang@gmail.com</p>
+                            <a href="#" className="btn-contact">See my Portfolio</a>
                         </div>
                     </div>
                 </div>
