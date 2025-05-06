468a81d144c4804283968aea40c9080fcc16d83f
Patinyaporn <cosyprang@gmail.com>
2025-05-06 09:42:50 +0700

    fix sitebar layout

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -22,10 +22,10 @@ function Nav() {
                         <i className="fa-solid fa-bars" onClick={toggleMenu}></i>
                     </div>
                     <div className="social-menu-header">
-                        <a href="#">
+                        <a href="https://www.facebook.com/patinyaporn.sudtho.2024">
                             <i className="fa-brands fa-square-facebook"></i>
                         </a>
-                        <a href="#">
+                        <a href="https://www.instagram.com/23rd.prxng/">
                             <i className="fa-brands fa-square-instagram"></i>
                         </a>
                         <a href="#">
